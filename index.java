e29b9f4507d058c73ef827ce862b4f0e70d665b7
unknown <samarthredij333@.com>
2025-07-02 19:47:20 +0530

    added index

---
+++ index.java
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tribute Page</title>
+   <link href="style.css" rel="stylesheet">
+</head>
+<body>
+
+   <nav class="navbar">
+  <div class="logo">Mumbai</div>
+  <ul class="nav-links">
+    <li><a href="#history">History</a></li>
+    <li><a href="#Famous">Famous for</a></li>
+    <li><a href="#images">Gallery</a></li>
+    <li><a href="#quotes">Quotes</a></li>
+  </ul>
+</nav>
+
+  <header>
+    <h1>Mumbai - The Heart of India.</h1>
+    <h2>"Dream City of Everyone"</h2>
+  </header>
+
+  <section id="history">
+    <h2><u> History </u></h2>
+    <p>Mumbai, also known as Bombay (its official name until 1995), is the capital city of the Indian state of Maharashtra. Mumbai is the financial capital and the most populous city proper of India with an estimated population of 12.5 million (1.25 crore). Mumbai is the centre of the Mumbai Metropolitan Region, which is among the most populous metropolitan areas in the world with a population of over 23 million (2.3 crore). Mumbai lies on the Konkan coast on the west coast of India and has a deep natural harbour. In 2008, Mumbai was named an alpha world city. Mumbai has the highest number of billionaires out of any city in Asia.</p>
+  </section>
+
+
+<section id="Famous">
+  <h2><u>Famous</u></h2>
+  <h1><u>Food</u></h1>
+  <div class="design-layout">
+    <div class="design-image">
+      <a href="https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://en.wikipedia.org/wiki/Vada_pav&ved=2ahUKEwjV_4Lvmp6OAxXhSmwGHVRQCqwQFnoECCgQAQ&usg=AOvVaw2-OYfYUgQKmHTlru0N_bay"> <img src="Vada Pav.jpg"></a>
+    </div>
+    <div class="design-text">
+      <p>Vada Pav, often called the Indian burger, is a popular Mumbai street food. It features a spicy potato fritter (vada) nestled inside a soft bread bun (pav), typically accompanied by chutneys and fried green chilies. The vada is made from mashed potatoes, spiced and deep-fried in a gram flour batter. 
+</p>
+    </div>
+  </div>
+</section>
+
+<section id="design">
+  <h1><u>Beach</u></h1>
+  <div class="design-layout">
+    <div class="design-text">
+      <p>Marine Drive is a prominent landmark in Mumbai, India, known as a C-shaped, 3.6-kilometer-long promenade along the Arabian Sea. It's a popular spot for locals and tourists, offering scenic views, especially at sunset when it's nicknamed the "Queen's Necklace" due to the way the streetlights illuminate the coastline. </p>
+    </div>
+<div class="design-image">
+       <a href="https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://en.wikipedia.org/wiki/Marine_Drive,_Mumbai&ved=2ahUKEwib4YD_mp6OAxVJRmwGHTocF8MQFnoECBgQAQ&usg=AOvVaw23tvHfk39CEDBPaBcRWXMR"><img src="Marine Drive.jpg"></a>
+    </div>
+  </div>
+</section>
+
+<section id="design">
+  <h1><u>Bollywood</u></h1>
+  <div class="design-layout">
+    <div class="design-image">
+       <a href="https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://en.wikipedia.org/wiki/Hindi_cinema&ved=2ahUKEwjx6N-Hm56OAxWZRWcHHc5UHYkQFnoECEEQAQ&usg=AOvVaw1O9cGlKNO-GtZY0GdPvHO5"><img src="Bollywood.jpg"></a>
+    </div>
+    <div class="design-text">
+      <p>Hindi cinema, popularly known as Bollywood and formerly as Bombay cinema, is primarily produced in Mumbai. The popular term Bollywood is a portmanteau of "Bombay" (former name of Mumbai) and "Hollywood". The industry, producing films in the Hindi language, is a part of the larger Indian cinema industry, which also includes South Indian cinema and other smaller film industries.</p>  
+    </div>
+  </div>
+</section>
+
+
+  <section id="images">
+    <h2><u>Specality of Mumbai</u></h2>
+    <div class="gallery">
+      <a href="https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://en.wikipedia.org/wiki/Gateway_of_India&ved=2ahUKEwjnkJaXm56OAxWbd2wGHfroL_4QFnoECGIQAQ&usg=AOvVaw3dO_dPOn5AIqkgGGKwPiEq"><img src="m1.jpg"></a>
+      <a href="https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://en.wikipedia.org/wiki/Mumbai_Suburban_Railway&ved=2ahUKEwio26Ogm56OAxXlVmwGHcf3ARUQFnoECE4QAQ&usg=AOvVaw1w77mIFnZsC89toJZoYGWW"> <img src="m2.jpg"></a>
+       <a href="https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://en.wikipedia.org/wiki/Elephanta_Caves&ved=2ahUKEwiC2Nypm56OAxUdRWcHHTVnFxAQFnoECFAQAQ&usg=AOvVaw2XKLNwzXLLwppKTo0FDR5d"><img src="m3.jpg"></a>
+       <a href="https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://en.wikipedia.org/wiki/Mumbai&ved=2ahUKEwjj_p-zm56OAxWKcGwGHUMrJ_EQFnoECEoQAQ&usg=AOvVaw08bAvqF4_7gZaVBgO1vzaG"><img src="m4.jpg"></a>
+    </div>
+  </section>
+
+  <section id="quotes">
+    <h2><u>Why Everyone Loves Mumbai...</u></h2>
+    <div class="quote">“The City That Never Sleeps.”</div>
+    <div class="quote">“Where Dreams Come Alive.”</div>
+    <div class="quote">“The City of Opportunities.”</div>
+    <div class="quote">“Where History Meets Modernity.”</div>
+    <div class="quote">“The Gateway to India.”</div>
+  </section>
+
+<a href="#" class="go-to-top" title="Back to Top"> Go To Top</a>
+
+  <footer>
+    <a class="button" href="https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://en.wikipedia.org/wiki/Mumbai&ved=2ahUKEwjj_p-zm56OAxWKcGwGHUMrJ_EQFnoECEoQAQ&usg=AOvVaw08bAvqF4_7gZaVBgO1vzaG">Explore More</a>
+    <p>&copy; Mumbai 2025.  All rights reserved. - Samarth Redij</p>
+  </footer>
+
+</body>
+</html>
